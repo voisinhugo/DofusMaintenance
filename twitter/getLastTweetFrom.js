7b908baa5036e83636e0b53d3382fadffade1159
Hugo Voisin <hugov@bam.tech>
2020-11-03 00:25:35 +0100

    feat: get last tweet which is not a retweet

--- twitter/getLastTweetFrom.js
+++ twitter/getLastTweetFrom.js
@@ -4,7 +4,7 @@ const twitterCredentials = require('./auth.json');
 const BEARER_TOKEN = twitterCredentials.bearer_token;
 
 const getLastTweetFrom = async (twitterAccount) => {
-  const url = `https://api.twitter.com/2/tweets/search/recent?query=from:${twitterAccount}`;
+  const url = `https://api.twitter.com/2/tweets/search/recent?query=from:${twitterAccount}&tweet.fields=referenced_tweets`;
   const params = {
     headers: {
       Authorization: `Bearer ${BEARER_TOKEN}`
@@ -12,7 +12,9 @@ const getLastTweetFrom = async (twitterAccount) => {
   };
 
   const response = await axios.get(url, params);
-  return response.data.data[0];
+  const tweets = response.data.data;
+  const notRetweetTweets = tweets.filter((tweet) => !tweet.referenced_tweets);
+  return notRetweetTweets[0];
 };
 
 exports.getLastTweetFrom = getLastTweetFrom;
